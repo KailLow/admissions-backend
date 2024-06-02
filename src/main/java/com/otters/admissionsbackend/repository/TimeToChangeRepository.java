a2ef7c96f36a4ff522a2db5bb948e76f9949bd51
tonyanhkhoi <tonysquadgamer@gmail.com>
2024-06-03 00:17:40 +0700

    <dev> add time to change

--- src/main/java/com/otters/admissionsbackend/repository/TimeToChangeRepository.java
+++ src/main/java/com/otters/admissionsbackend/repository/TimeToChangeRepository.java
@@ -1,2 +1,7 @@
-package com.otters.admissionsbackend.repository;public interface TimeToChangeRepository {
+package com.otters.admissionsbackend.repository;
+
+import com.otters.admissionsbackend.model.TimeToChange;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TimeToChangeRepository extends JpaRepository<TimeToChange, String> {
 }
