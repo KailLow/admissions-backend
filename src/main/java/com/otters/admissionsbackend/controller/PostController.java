5808f5ad6a6ed98b3ca0407f764a3b68042ac343
tonyanhkhoi <tonysquadgamer@gmail.com>
2024-06-05 17:32:26 +0700

    <Dev> add notification and Post

--- src/main/java/com/otters/admissionsbackend/controller/PostController.java
+++ src/main/java/com/otters/admissionsbackend/controller/PostController.java
@@ -1,2 +1,48 @@
-package com.otters.admissionsbackend.controller;public class PostController {
+package com.otters.admissionsbackend.controller;
+
+import com.otters.admissionsbackend.model.Post;
+import com.otters.admissionsbackend.service.PostService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class PostController {
+    private final PostService service;
+
+    public PostController(PostService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/post")
+    public ResponseEntity<List<Post>> findAll() {
+        return ResponseEntity.ok(service.findAll());
+    }
+
+    @GetMapping("/post/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        return ResponseEntity.ok(service.findById(id));
+    }
+
+    @PostMapping("admin/post")
+    public ResponseEntity<Post> add(
+            @RequestBody Post request
+    ) {
+        return ResponseEntity.ok(service.add(request));
+    }
+
+    @DeleteMapping("admin/post/{id}")
+    public ResponseEntity<?> remove(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("admin/post/{id}")
+    public ResponseEntity<?> update(
+            @PathVariable String id,
+            @RequestBody Post request
+    ) {
+        return ResponseEntity.ok(service.update(id, request));
+    }
 }
